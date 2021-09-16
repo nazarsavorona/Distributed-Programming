731d10d01b318d006c73cdfec1820bbe1d6b9e22
nazarsavorona <nazar.savorona@gmail.com>
2021-09-16 21:45:27 +0300

    Add lab2.b part

---
+++ src/dp/lab2/b/Truck.java
@@ -0,0 +1,28 @@
+package dp.lab2.b;
+
+public class Truck extends Buffer {
+    private int lastGivenGoodIndex;
+
+    public Truck() {
+        super();
+        this.lastGivenGoodIndex = -1;
+    }
+
+    @Override
+    public synchronized String getGood() {
+        String goodToPass = null;
+
+        if (goods.size() > lastGivenGoodIndex + 1) {
+            goodToPass = goods.get(++lastGivenGoodIndex);
+        }
+
+        isBusy = false;
+
+        return goodToPass;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return goods.size() == lastGivenGoodIndex + 1;
+    }
+}
