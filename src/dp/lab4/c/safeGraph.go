4f4d0ecd2a0203f809ffc7df2fd5dffa8058d4a6
nazarsavorona <nazar.savorona@gmail.com>
2021-09-30 22:13:26 +0300

    Add lab4.c part

---
+++ src/dp/lab4/c/safeGraph.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type SafeGraph struct {
+	container Graph
+	mutex     sync.Mutex
+}
+
+func (graph *SafeGraph) changeRoutePrice(firstCity, secondCity string, newPrice int) {
+	graph.mutex.Lock()
+	graph.container.changePrice(firstCity, secondCity, newPrice)
+	graph.mutex.Unlock()
+}
+
+func (graph *SafeGraph) addRoute(firstCity, secondCity string, price int) {
+	graph.mutex.Lock()
+	graph.container.addEdge(firstCity, secondCity, price)
+	graph.mutex.Unlock()
+}
+
+func (graph *SafeGraph) deleteRoute(firstCity, secondCity string) {
+	graph.mutex.Lock()
+	graph.container.deleteEdge(firstCity, secondCity)
+	graph.mutex.Unlock()
+}
+
+func (graph *SafeGraph) addCity(city string) {
+	graph.mutex.Lock()
+	graph.container.addVertex(city)
+	graph.mutex.Unlock()
+}
+
+func (graph *SafeGraph) deleteCity(city string) {
+	graph.mutex.Lock()
+	graph.container.deleteVertex(city)
+	graph.mutex.Unlock()
+}
+
+func (graph *SafeGraph) getPathCost(firstCity, secondCity string) {
+	graph.mutex.Lock()
+	exists, cost := graph.container.getPathCost(firstCity, secondCity)
+	graph.mutex.Unlock()
+
+	if exists {
+		fmt.Printf("\"%s\" -> \"%s\" route cost is %d\n", firstCity, secondCity, cost)
+	} else {
+		fmt.Printf("There is no route between \"%s\" and \"%s\"\n", firstCity, secondCity)
+	}
+}
+
+func (graph *SafeGraph) printGraphviz(name string) {
+	graph.mutex.Lock()
+	println(graph.container.getGraphvizInfo(name))
+	graph.mutex.Unlock()
+}
